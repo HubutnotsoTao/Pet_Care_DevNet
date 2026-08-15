b10a1170f7dac4be5ea81979e63843ed3c158352
Chxxrz <jbpecson3@gmail.com>
2026-08-15 10:35:24 +0000

    Partially finished Add Owner function

--- src/owner.py
+++ src/owner.py
@@ -1,12 +1,19 @@
 #Add owners
 
-def add_owners(owner_name, owner_age, owner_contact):
+def add_owners():
     owner_name = input("Enter Name: ")
     owner_age = input("Enter Age: ")
     owner_contact = input("Enter Contact #: ")
+    pet_name = input("Pet Name: ")
+    pet_type = input("Type of Pet: ")
 
+    print()
+    print ("=====Add Owners=====")
     print (f"Name: {owner_name}")
     print (f"Age: {owner_age}")
     print (f"Contact: {owner_contact}")
+    print ("=====Pet Details=====")
+    print (f"Name of Pet: {pet_name}")
+    print (f"Type of Pet: {pet_type}")
 
-add_owners("John", "23", "09999421")
+add_owners()
