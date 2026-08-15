05158f7cb88372185afef5647f76c2292bc1ce15
Chxxrz <jbpecson3@gmail.com>
2026-08-15 06:34:28 +0000

    Unfinished

---
+++ src/owner.py
@@ -0,0 +1,12 @@
+#Add owners
+
+def add_owners(owner_name, owner_age, owner_contact):
+    owner_name = input("Enter Name: ")
+    owner_age = input("Enter Age: ")
+    owner_contact = input("Enter Contact #: ")
+
+    print (f"Name: {owner_name}")
+    print (f"Age: {owner_age}")
+    print (f"Contact: {owner_contact}")
+
+add_owners("John", "23", "09999421")
