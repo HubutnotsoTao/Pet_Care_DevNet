eb5b615e42313c52d9a7570df528d2038dcafffc
Macapinlac, Jeremiah S. <jeriahisthe1@gmail.com>
2026-08-15 06:34:30 +0000

    ang cute ng pet mo

---
+++ src/pet.py
@@ -0,0 +1,14 @@
+def add_pet(pet_name, animal_kind, pet_age, pet_owner, stay_duration):
+    pet_name = input("Enter pet name:")
+    pet_age = input("Enter pet age:")
+    animal_kind = input("Enter what kind of animal is your pet:")
+    pet_owner = input("Enter pet owner name:")
+    stay_duration = input("Enter pet stay duration:")
+    print (f"Pet Name:{pet_name}")
+    print (f"pet animal kind:{animal_kind}")
+    print (f"Pet Age:{pet_age}")
+    print (f"Pet Owner:{pet_owner}")
+    print (f"Pet stay  duration:{stay_duration}")
+
+
+    
