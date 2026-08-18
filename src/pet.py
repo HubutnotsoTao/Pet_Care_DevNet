4f8e75d3f95aeef1babf60de8df12b415bc4f3b4
Justine Paul <lastg3n0@gmail.com>
2026-08-18 14:31:03 +0000

    updated code in pet and owner to ensure compatibility

--- src/pet.py
+++ src/pet.py
@@ -1,15 +1,31 @@
-def add_pet():
-    pet_name = input("Enter pet name:")
-    pet_age = input("Enter pet age:")
-    animal_kind = input("Enter type of pet:")
-    pet_owner = input("Enter pet owner name:")
-    stay_duration = input("Enter pet stay duration:")
-    print(f"=====ADDED DETAILS=====")
-    print (f"Pet Name:{pet_name}")
-    print (f"Pet Age:{pet_age}")
-    print (f"Type of Pet:{animal_kind}")
-    print (f"Pet Owner:{pet_owner}")
-    print (f"Pet stay  duration:{stay_duration}")
+#Add pets
 
-add_pet()
-    
+from data import pets
+
+def add_pet():
+
+    pet_name = input("Enter pet name:").strip()
+    pet_age = input("Enter pet age:").strip()
+    animal_kind = input("Enter type of pet:").strip()
+    pet_owner = input("Enter pet owner name:").strip()
+    stay_duration = input("Enter pet stay duration in hours:").strip()
+
+    print(f"=====ADDED DETAILS=====")
+    print(f"Pet Name: {pet_name}")
+    print(f"Pet Age: {pet_age}")
+    print(f"Type of Pet: {animal_kind}")
+    print(f"Pet Owner: {pet_owner}")
+    print(f"Pet stay duration: {stay_duration}")
+
+    pet_record = {
+        "name": pet_name,
+        "age": pet_age,
+        "kind": animal_kind,
+        "owner": pet_owner,
+        "stay_duration": stay_duration,
+        "total_charge": stay_duration*2
+    }
+
+    pets.append(pet_record)
+
+
