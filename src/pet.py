be36579deaf9b3fd504b0da5c417bf5fcf0c7d16
Macapinlac, Jeremiah S. <jeriahisthe1@gmail.com>
2026-08-18 09:20:17 +0000

    Modified file

--- src/pet.py
+++ src/pet.py
@@ -1,4 +1,3 @@
-def add_pet(pet_name, pet_age,  animal_kind, animal_kind, stay_duration):
     def add_pet():
     pet_name = input("Enter pet name:")
     pet_age = input("Enter pet age:")
@@ -11,5 +10,5 @@ def add_pet(pet_name, pet_age,  animal_kind, animal_kind, stay_duration):
     print (f"Pet Owner:{pet_owner}")
     print (f"Pet stay  duration:{stay_duration}")
 
-add_pet()
+    add_pet()
     
