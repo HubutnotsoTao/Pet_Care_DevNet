72cc4e4efdbb480a7215582060959c3d60bbb022
Justine Paul <lastg3n0@gmail.com>
2026-08-18 11:37:07 +0000

    added layout for navigation

--- src/navigation.py
+++ src/navigation.py
@@ -1,31 +1,47 @@
 def actions():
-    global choice
-    print("Welcome Rodney's Pet Care!")
-    print("[1]Pet Care Details \n[2]Display Current Pets \n[3]Display List of Owners\n")    
-    choice = input("Choose your Action: ").strip()
-    match choice:
-        case 1:
-            pet_care_details()
+    
+    while True: #navigation loop
+        print ("-"*40)
+        print("Welcome Rodney's Pet Care!\n")
+        print("[1]Pet Care Details \n[2]Display Current Pets \n[3]Display List of Owners\n[4]Exit")   
+        print ("-"*40) 
+        choice = input("Choose your Action: ").strip()
 
-        case 2:
-            pet_display()
-
-        case 3:
-            owner_display()
-
-        case _:
-            print("Please select a valid option")
+    #Error Catching
+        try:
+            choice = int(choice)
+        except ValueError:
+            print ("Please select a valid option!")
             actions()
+            return
+
+        match choice:
+            case 1:
+                pet_care_details()
+
+            case 2:
+                pet_display()
+
+            case 3:
+                owner_display()
+
+            case 4:
+                print("Thank you for visiting Rodney's Pet Care!")
+                return
+
+            case _:
+                print("Please select a valid option")
+        
 
 def pet_care_details():
     print("Welcome to Rodney's Pet Care!")
     print("It is our mission to provide the best pet care in this city")
-    print("Our rate is $2 per hour")
+    print("Our rate is $2 per hour\n")
 
 def pet_display():
-    print("Pets being cared for")
+    print("Pets being cared for\n")
 
 def owner_display():
-    print("List of owners")
+    print("List of owners\n")
 
 actions()
